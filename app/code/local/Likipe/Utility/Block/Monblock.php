3dbbc945f73616e2b46b7eb3e803c5e53623de1c
Alex <alex@likipe.se>
2014-12-23 10:59:59 +0700

    admin form

--- app/code/local/Likipe/Utility/Block/Monblock.php
+++ app/code/local/Likipe/Utility/Block/Monblock.php
@@ -1,10 +1,6 @@
 <?php
 class Likipe_Utility_Block_Monblock extends Mage_Core_Block_Template
 {
-	public function _construct()
-	{
-		var_dump(123);
-	}
 	public function methodblock()
 	{
 		return "informations about my block AAA !!" ;
