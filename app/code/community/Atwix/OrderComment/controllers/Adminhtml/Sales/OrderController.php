ac94a006be77aaa77cc0d900a1d4a695aedfc234
Alex <alex@likipe.se>
2014-12-30 17:53:05 +0700

    add comment

---
+++ app/code/community/Atwix/OrderComment/controllers/Adminhtml/Sales/OrderController.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Atwix
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category    Atwix
+ * @package     Atwix_OrderComment
+ * @author      Atwix Core Team
+ * @copyright   Copyright (c) 2012 Atwix (http://www.atwix.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+require_once 'Mage/Adminhtml/controllers/Sales/OrderController.php';
+class Atwix_OrderComment_Adminhtml_Sales_OrderController extends Mage_Adminhtml_Sales_OrderController
+{
+    /**
+     * Add order comment action
+     */
+    public function addCommentAction()
+    {
+        if ($order = $this->_initOrder()) {
+            try {
+                $response = false;
+                $data = $this->getRequest()->getPost('history');
+                $notify = isset($data['is_customer_notified']) ? $data['is_customer_notified'] : false;
+                $visible = isset($data['is_visible_on_front']) ? $data['is_visible_on_front'] : false;
+
+                //getting username
+                $user = Mage::getSingleton('admin/session');
+                $username = $user->getUser()->getUsername();
+                $append = " [name](by ".$username.")[/name]";
+
+                //appending username with markup to comment
+                $order->addStatusHistoryComment($data['comment'].$append, $data['status'])
+                    ->setIsVisibleOnFront($visible)
+                    ->setIsCustomerNotified($notify);
+
+                $comment = trim(strip_tags($data['comment']));
+
+                $order->save();
+                $order->sendOrderUpdateEmail($notify, $comment);
+
+                $this->loadLayout('empty');
+                $this->renderLayout();
+            }
+            catch (Mage_Core_Exception $e) {
+                $response = array(
+                    'error'     => true,
+                    'message'   => $e->getMessage(),
+                );
+            }
+            catch (Exception $e) {
+                $response = array(
+                    'error'     => true,
+                    'message'   => $this->__('Cannot add order history.')
+                );
+            }
+            if (is_array($response)) {
+                $response = Mage::helper('core')->jsonEncode($response);
+                $this->getResponse()->setBody($response);
+            }
+        }
+    }
+}
