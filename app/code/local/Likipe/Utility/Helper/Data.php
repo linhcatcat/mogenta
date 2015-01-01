0856245ebfefd87f6b94417da941bdbf863db11f
Tran Cat Linh <trancatlinh@gmail.com>
2015-01-01 10:05:32 +0700

    resize image

--- app/code/local/Likipe/Utility/Helper/Data.php
+++ app/code/local/Likipe/Utility/Helper/Data.php
@@ -3,4 +3,29 @@ class Likipe_Utility_Helper_Data extends Mage_Core_Helper_Abstract{
 	public function bytwo($nbr){
 		return $nbr*2;
 	}
+
+	public function resizeImg($fileName, $width, $height = '')
+	{
+	    $folderURL = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_MEDIA);
+	    $imageURL = $folderURL . $fileName;
+	 
+	    $basePath = Mage::getBaseDir(Mage_Core_Model_Store::URL_TYPE_MEDIA) . DS . $fileName;
+	    $newPath = Mage::getBaseDir(Mage_Core_Model_Store::URL_TYPE_MEDIA) . DS . "resized" . DS . $fileName;
+	    //if width empty then return original size image's URL
+	    if ($width != '') {
+	        //if image has already resized then just return URL
+	        if (file_exists($basePath) && is_file($basePath) && !file_exists($newPath)) {
+	            $imageObj = new Varien_Image($basePath);
+	            $imageObj->constrainOnly(TRUE);
+	            $imageObj->keepAspectRatio(FALSE);
+	            $imageObj->keepFrame(FALSE);
+	            $imageObj->resize($width, $height);
+	            $imageObj->save($newPath);
+	        }
+	        $resizedURL = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_MEDIA) . "resized" . DS . $fileName;
+	     } else {
+	        $resizedURL = $imageURL;
+	     }
+	     return $resizedURL;
+	}
 }
