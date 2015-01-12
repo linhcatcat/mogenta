ca65e2e317523bd397c78ad9081df05184ed27f6
Alex <alex@likipe.se>
2015-01-12 14:16:45 +0700

    custom invoice pdf

---
+++ app/code/local/Inchoo/Invoice/Model/Order/Pdf/Shipment.php
@@ -0,0 +1,52 @@
+<?php
+class Inchoo_Invoice_Model_Order_Pdf_Shipment extends Mage_Sales_Model_Order_Pdf_Shipment  
+{
+	// code from original class (I have removed it for readability purposes)
+	 
+	//add this method 
+	public function addGiftMsg($page, $giftMessageSender, $giftMessageNote) 
+	{
+		if(empty($giftMessageNote)) {
+			return;
+		}
+		$pipfmText = $giftMessageSender ."***BREAK***"."  "."***BREAK***".wordwrap($giftMessageNote, 100, "***BREAK***", true);
+
+		$pipfmTextLines = array();
+		$pipfmTextLines = explode("***BREAK***", $pipfmText);
+
+
+
+		$i = 0;
+
+		$pipfmTextLineStartY = 300;
+		foreach ($pipfmTextLines as $pipfmTextLine) 
+		{
+			$i ++;
+
+			//Bold only the first line
+			if($i == 1) 
+			{
+				$this->_setFontBold_Modified($page, 10);
+
+
+			} else {
+				$this->_setFontRegular($page, 10);
+			}
+
+			$page->drawText($pipfmTextLine, 60, $pipfmTextLineStartY, 'UTF-8');
+			$pipfmTextLineStartY = $pipfmTextLineStartY - 10;	
+
+		}
+	}
+	 
+	// at some place where you want output (inside getPdf() method) for it just drop this lines:
+	/*public function getPdf($shipments = array())
+	{
+		// ORIGINAL CODE REMOVED FOR READABILITY PURPOSES
+
+		$giftMessage = Mage::getModel("giftmessage/message")->load(35);//$order->getGiftMessageId());
+		$giftMessageSender ="Message from ".$giftMessage->getSender().':';
+		$giftMessageNote = $giftMessage->getMessage();
+		$this->addGiftMsg($page, $giftMessageSender, $giftMessageNote);
+	}*/
+}
