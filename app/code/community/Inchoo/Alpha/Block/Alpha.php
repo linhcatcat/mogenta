96150860c74122c4bcf8109bd81c93cc73314e6e
Alex <alex@likipe.se>
2015-01-21 15:15:33 +0700

    general extent

---
+++ app/code/community/Inchoo/Alpha/Block/Alpha.php
@@ -0,0 +1,13 @@
+<?php
+
+class Inchoo_Alpha_Block_Alpha extends Mage_Core_Block_Template
+{
+    /**
+     * Constructor. Set template.
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setTemplate('inchoo/alpha/alpha.phtml');
+    }
+}
